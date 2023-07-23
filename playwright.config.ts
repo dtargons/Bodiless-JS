814d6c5a781aa6a5528a2caa93be650c7e9a5c1b
Dzmitry Targonsky <dmitry.targonsky@gmail.com>
2023-07-23 20:05:35 +0400

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ const configurators = {
     baseConfig.testDir = './playwright/tests/starter';
     baseConfig.webServer = {
       ...defaultServerConfig,
-      command: 'cd new/sites/starter-site && npm run serve:test',
+      command: 'cd new/sites/starter-site && npm start',
     };
     baseConfig.globalSetup = require.resolve('./playwright/tests/setup/setup.ts');
   },
